41beb2a0cdcb5c6e25aa994766ac23dd8bdf00a0
Jonathan Takahashi <jonathan.takahashi@cvisionconsulting.com>
2018-06-20 14:56:18 -0400

    Add header containing error codes.

---
+++ deploy/include/error_codes.h
@@ -0,0 +1,23 @@
+/// @file
+/// @brief Contains definitions of error codes.
+
+#ifndef OPENEM_DEPLOY_ERROR_CODES_H_
+#define OPENEM_DEPLOY_ERROR_CODES_H_
+
+namespace openem {
+
+/// All openem error codes.
+enum ErrorCode {
+  kSuccess = 0,       ///< No error.
+  kErrorLoadingModel, ///< Failed to read tensorflow model.
+  kErrorGraphDims,    ///< Wrong number of dimensions in graph input node.
+  kErrorNoShape,      ///< No shape attribute in graph input node.
+  kErrorTfSession,    ///< Failed to create tensorflow session.
+  kErrorTfGraph,      ///< Failed to create tensorflow graph.
+  kErrorBadInit       ///< Attempted to use uninitialized object.
+};
+
+} // namespace openem
+
+#endif // OPENEM_DEPLOY_ERROR_CODES_H_
+

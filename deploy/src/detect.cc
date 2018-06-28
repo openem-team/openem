bf1942ad3fe95562a4e1aa1b779f4433efa4444e
Jonathan Takahashi <jonathan.takahashi@cvisionconsulting.com>
2018-06-28 12:01:05 -0400

    Switch to using NMS function in opencv dnn module

--- deploy/src/detect.cc
+++ deploy/src/detect.cc
@@ -19,29 +19,47 @@ namespace {
 /// @param anchors Anchor box parameters, one box per row.
 /// @param variances Variances corresponding to each anchor box param.
 /// @return Decoded bounding boxes.
-cv::Mat DecodeBoxes(
+std::vector<cv::Rect> DecodeBoxes(
     const cv::Mat& loc, 
     const cv::Mat& anchors, 
-    const cv::Mat& variances) {
+    const cv::Mat& variances,
+    const cv::Size& img_size) {
+  std::vector<cv::Rect> decoded;
   cv::Mat anchor_width, anchor_height, anchor_center_x, anchor_center_y;
   cv::Mat decode_width, decode_height, decode_center_x, decode_center_y;
+  cv::Mat decode_x0, decode_y0, decode_x1, decode_y1;
   anchor_width = anchors.col(2) - anchors.col(0);
   anchor_height = anchors.col(3) - anchors.col(1);
   anchor_center_x = 0.5 * (anchors.col(2) + anchors.col(0));
   anchor_center_y = 0.5 * (anchors.col(3) + anchors.col(1));
-  decode_center_x = loc.col(0) * anchor_width * variances.col(0);
+  decode_center_x = loc.col(0).mul(anchor_width).mul(variances.col(0));
   decode_center_x += anchor_center_x;
-  decode_center_y = loc.col(1) * anchor_width * variances.col(1);
-  cv::exp(loc.col(2) * variances.col(2), decode_width);
-  decode_width *= anchor_width;
-  cv::exp(loc.col(3) * variances.col(3), decode_height);
-  decode_height *= anchor_height;
-  cv::Mat decode_bbox(loc.rows, 4, CV_32F);
-  decode_bbox.col(0) = decode_center_x - 0.5 * decode_width;
-  decode_bbox.col(1) = decode_center_y - 0.5 * decode_height;
-  decode_bbox.col(2) = decode_center_x + 0.5 * decode_width;
-  decode_bbox.col(3) = decode_center_y + 0.5 * decode_height;
-  return decode_bbox;
+  decode_center_y = loc.col(1).mul(anchor_height).mul(variances.col(1));
+  decode_center_y += anchor_center_y;
+  cv::exp(loc.col(2).mul(variances.col(2)), decode_width);
+  decode_width = decode_width.mul(anchor_width);
+  cv::exp(loc.col(3).mul(variances.col(3)), decode_height);
+  decode_height = decode_height.mul(anchor_height);
+  decode_x0 = (decode_center_x - 0.5 * decode_width) * img_size.width;
+  decode_y0 = (decode_center_y - 0.5 * decode_height) * img_size.height;
+  decode_x1 = (decode_center_x + 0.5 * decode_width) * img_size.width;
+  decode_y1 = (decode_center_y + 0.5 * decode_height) * img_size.height;
+  decode_x0.setTo(0, decode_x0 < 0);
+  decode_x0.setTo(img_size.width, decode_x0 >= img_size.width);
+  decode_y0.setTo(0, decode_y0 < 0);
+  decode_y0.setTo(img_size.height, decode_y0 >= img_size.height);
+  decode_x1.setTo(0, decode_x1 < 0);
+  decode_x1.setTo(img_size.width, decode_x1 >= img_size.width);
+  decode_y1.setTo(0, decode_y1 < 0);
+  decode_y1.setTo(img_size.height, decode_y1 >= img_size.height);
+  for (int i = 0; i < loc.rows; ++i) {
+    decoded.emplace_back(
+        decode_x0.at<float>(i),
+        decode_y0.at<float>(i),
+        decode_x1.at<float>(i) - decode_x0.at<float>(i) + 1,
+        decode_y1.at<float>(i) - decode_y0.at<float>(i) + 1);
+  }
+  return decoded;
 }
   
 } // namespace
@@ -92,29 +110,26 @@ ErrorCode Detector::Process(std::vector<std::vector<cv::Rect>>* detections) {
   int conf_stop = outputs.back().dim_size(2) - 8;
   int anc_stop = conf_stop + 4;
   int var_stop = anc_stop + 4;
-  cv::Mat loc, conf, variances, anchors, decoded;
+  cv::Mat loc, conf, variances, anchors;
+  std::vector<cv::Rect> boxes;
+  std::vector<float> scores;
+  std::vector<int> indices;
+  double min, max;
   for (const auto& p : pred) {
-    std::vector<cv::Rect> dets;
     loc = p(cv::Range::all(), cv::Range(0, pred_stop));
     conf = p(cv::Range::all(), cv::Range(pred_stop, conf_stop));
+    cv::minMaxLoc(loc, &min, &max);
     anchors = p(cv::Range::all(), cv::Range(conf_stop, anc_stop));
     variances = p(cv::Range::all(), cv::Range(anc_stop, var_stop));
-    decoded = DecodeBoxes(loc, anchors, variances);
+    boxes = DecodeBoxes(loc, anchors, variances, impl_->model_.ImageSize());
+    std::vector<cv::Rect> dets;
     for (int c = 0; c < conf.cols; ++c) {
-      cv::Mat over_thresh;
-      cv::threshold(conf.col(c), over_thresh, 0.01, 1.0, cv::THRESH_BINARY);
-      int total = std::lround(cv::sum(over_thresh)[0]);
-      cv::Mat boxes(total, 4, CV_32F);
-      cv::Mat confs(total, 1, CV_32F);
-      for (int i = 0, k = 0; i < total; ++i) {
-        if (over_thresh.at<float>(i) > 0.5) {
-          boxes.row(k) = decoded.row(i);
-          confs.row(k) = conf.row(i);
-          ++k;
-        }
+      cv::Mat& c_conf = conf.col(c);
+      scores.assign(c_conf.begin<float>(), c_conf.end<float>());
+      cv::dnn::NMSBoxes(boxes, scores, 0.01, 0.45, indices, 1.0, 200);
+      for (int idx : indices) {
+        dets.push_back(boxes[idx]);
       }
-      tf::Tensor t_boxes = util::MatToTensor(boxes, {boxes.rows, boxes.cols});
-      tf::Tensor t_confs = util::MatToTensor(confs, {confs.rows});
     }
     detections->push_back(std::move(dets));
   }

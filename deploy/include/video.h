d47d0d3736c0edc152d26a6ef9ea3ac4facf8db9
Jonathan Takahashi <jonathan.takahashi@cvisionconsulting.com>
2018-07-26 21:26:43 -0400

    Add video writer class

--- deploy/include/video.h
+++ deploy/include/video.h
@@ -8,6 +8,16 @@
 
 namespace openem {
 
+/// Enum specifying codec.
+enum Codec {
+  kRaw,  ///< No codec.
+  kH264, ///< H264.
+  kH265, ///< H265.
+  kMjpg, ///< MJPEG.
+  kMrle, ///< Microsoft RLE.
+  kWmv2  ///< Windows media video 8.
+};
+
 /// Class for video playback.
 ///
 /// This class is a thin wrapper around a cv::VideoCapture.  This is
@@ -35,6 +45,9 @@ class VideoReader {
 
   /// Returns video height.
   int Height() const;
+
+  /// Returns frame rate.
+  double FrameRate() const;
  private:
   /// Forward declaration of implementation class.
   class VideoReaderImpl;
@@ -43,6 +56,45 @@ class VideoReader {
   std::unique_ptr<VideoReaderImpl> impl_;
 };
 
+/// Class for video recording.
+///
+/// This class is a thin wrapper around a cv::VideoWriter.  This is
+/// provided to remain SWIG-friendly and for compatibility with
+/// the OpenEM image class.
+class VideoWriter {
+ public:
+  /// Constructor.
+  VideoWriter();
+
+  /// Destructor.
+  ~VideoWriter();
+
+  /// Initializes the writer with the given video path, fps, codec, and 
+  /// frame size.
+  /// @param video_path Full qualified path to output video file.
+  /// @param fps Frame rate.
+  /// @param codec Codec.
+  /// @param size Size of the video.
+  /// @return Error code.
+  ErrorCode Init(
+      const std::string& video_path, 
+      double fps, 
+      Codec codec, 
+      const std::pair<int, int>& size);
+
+  /// Adds a frame for writing.
+  /// @param frame Video frame to be written.
+  /// @return Error code.
+  ErrorCode AddFrame(const Image& frame);
+ private:
+   /// Forward declaration of implementation class.
+   class VideoWriterImpl;
+
+   /// Pointer to implementation.
+   std::unique_ptr<VideoWriterImpl> impl_;
+};
+
 } // namespace openem
 
 #endif // OPENEM_DEPLOY_VIDEO_H_
+
--- deploy/src/video.cc
+++ deploy/src/video.cc
@@ -18,6 +18,19 @@ class VideoReader::VideoReaderImpl {
   cv::VideoCapture cap_;
 };
 
+/// Implementation details for VideoWriter.
+class VideoWriter::VideoWriterImpl {
+ public:
+  /// Constructor.
+  VideoWriterImpl();
+
+  /// Video writer doesn't store the size.
+  cv::Size size_;
+
+  /// Under the hood it's just a cv::VideoWriter.
+  cv::VideoWriter writer_;
+};
+
 VideoReader::VideoReaderImpl::VideoReaderImpl() : cap_() {}
 
 VideoReader::VideoReader() : impl_(new VideoReaderImpl()) {}
@@ -44,6 +57,51 @@ int VideoReader::Width() const {
 
 int VideoReader::Height() const {
   return static_cast<int>(impl_->cap_.get(cv::CAP_PROP_FRAME_HEIGHT));
+}
+
+double VideoReader::FrameRate() const {
+  return impl_->cap_.get(cv::CAP_PROP_FPS);
+}
+
+VideoWriter::VideoWriterImpl::VideoWriterImpl() : writer_() {}
+
+VideoWriter::VideoWriter() : impl_(new VideoWriterImpl()) {}
+
+VideoWriter::~VideoWriter() {}
+
+ErrorCode VideoWriter::Init(
+    const std::string& video_path, 
+    double fps, 
+    Codec codec, 
+    const std::pair<int, int>& size) {
+  int fourcc = 0;
+  switch (codec) {
+    case kH264:
+      fourcc = cv::VideoWriter::fourcc('H', '2', '6', '4');
+      break;
+    case kH265:
+      fourcc = cv::VideoWriter::fourcc('H', '2', '6', '5');
+      break;
+    case kMjpg:
+      fourcc = cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
+      break;
+    case kMrle:
+      fourcc = cv::VideoWriter::fourcc('M', 'R', 'L', 'E');
+      break;
+    case kWmv2:
+      fourcc = cv::VideoWriter::fourcc('W', 'M', 'V', '2');
+  }
+  impl_->size_ = cv::Size(size.first, size.second);
+  bool ok = impl_->writer_.open(video_path, fourcc, fps, impl_->size_);
+  if (!ok) return kErrorVidFileOpen;
+  return kSuccess;
+}
+
+ErrorCode VideoWriter::AddFrame(const Image& frame) {
+  const cv::Mat* mat = detail::MatFromImage(&frame);
+  if (impl_->size_ != mat->size()) return kErrorVidFrame;
+  impl_->writer_.write(*mat);
+  return kSuccess;
 }
 
 } // namespace openem

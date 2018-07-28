cd6cd1aa4be65d119067f5093440a37d2b7b82be
Jonathan Takahashi <jonathan.takahashi@cvisionconsulting.com>
2018-07-28 14:16:05 -0400

    Add copyright and license to source and example files

--- deploy/include/error_codes.h
+++ deploy/include/error_codes.h
@@ -1,5 +1,19 @@
 /// @file
 /// @brief Contains definitions of error codes.
+/// @copyright Copyright (C) 2018 CVision AI.
+/// @license This file is part of OpenEM, released under GPLv3.
+//  OpenEM is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with OpenEM.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef OPENEM_DEPLOY_ERROR_CODES_H_
 #define OPENEM_DEPLOY_ERROR_CODES_H_

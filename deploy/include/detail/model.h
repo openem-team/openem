f72655d91af04d4cc978b210993a77b7860264ca
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2018-07-28 15:03:13 -0400

    Fix typo

--- deploy/include/detail/model.h
+++ deploy/include/detail/model.h
@@ -13,7 +13,7 @@
 //  GNU General Public License for more details.
 //
 //  You should have received a copy of the GNU General Public License
-//  along with this OpenEM.  If not, see <http://www.gnu.org/licenses/>.
+//  along with OpenEM.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef OPENEM_DEPLOY_MODEL_H_
 #define OPENEM_DEPLOY_MODEL_H_

8ac8b2a7a7fe2706b35bffb822aa593392f68949
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2018-08-13 07:51:34 -0400

    Add declaration of count namespace

---
+++ deploy/include/count.h
@@ -0,0 +1,76 @@
+/// @file
+/// @brief Interface for counting fish.
+/// @copyright Copyright (C) 2018 CVision AI.
+/// @license This file is part of OpenEM, released under GPLv3.
+//  OpenEM is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with OpenEM.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef OPENEM_DEPLOY_COUNT_H_
+#define OPENEM_DEPLOY_COUNT_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "detect.h"
+#include "classify.h"
+#include "error_codes.h"
+
+namespace openem {
+namespace count {
+
+/// Class for finding keyframes.
+class KeyframeFinder {
+ public:
+  /// Constructor.
+  KeyframeFinder();
+
+  /// Destructor.
+  ~KeyframeFinder();
+
+  /// Initializes the keyframe finder.
+  /// @param model_path Path to protobuf file containing model.
+  /// @param img_width Width of image input to detector.
+  /// @param img_height Height of image input to detector.
+  /// @param gpu_fraction Fraction of GPU memory that may be allocated to
+  /// this object.
+  /// @return Error code.
+  ErrorCode Init(
+      const std::string& model_path, 
+      int img_width,
+      int img_height,
+      double gpu_fraction=1.0);
+
+  /// Finds keyframes in a given sequence.
+  /// @param classifications Sequence of outputs from classifier.
+  /// @param detections Sequence of outputs from detector.
+  /// @param keyframes Vector of keyframes in the sequence.  The length of
+  /// this vector is the number of fish in the sequence.
+  /// @return Error code.
+  ErrorCode Process(
+      const std::vector<classify::Classification>& classifications,
+      const std::vector<detect::Detection>& detections,
+      std::vector<int>* keyframes);
+ private:
+  /// Forward declaration of implementation class.
+  class KeyframeFinderImpl;
+
+  /// Pointer to implementation.
+  std::unique_ptr<KeyframeFinderImpl> impl_;
+};
+
+} // namespace count
+} // namespace openem
+
+#endif // OPENEM_DEPLOY_COUNT_H_
+
---
+++ deploy/src/count.cc
@@ -0,0 +1,62 @@
+/// @file
+/// @brief Implementation for counting fish.
+/// @copyright Copyright (C) 2018 CVision AI.
+/// @license This file is part of OpenEM, released under GPLv3.
+//  OpenEM is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with OpenEM.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "count.h"
+
+#include "detail/model.h"
+#include "detail/util.h"
+
+namespace openem {
+namespace count {
+
+/// Implementation details for KeyframeFinder.
+class KeyframeFinder::KeyframeFinderImpl {
+ public:
+  /// Stores and processes the model.
+  detail::Model model_;
+
+  /// Stores image width.  Needed for feature normalization.
+  double width_;
+
+  /// Stores image height.  Needed for feature normalization.
+  double height_;
+};
+
+KeyframeFinder::KeyframeFinder() : impl_(new KeyframeFinderImpl()) {}
+
+KeyframeFinder::~KeyframeFinder() {}
+
+ErrorCode KeyframeFinder::Init(
+    const std::string& model_path, 
+    int img_width,
+    int img_height,
+    double gpu_fraction) {
+  impl_->width_ = img_width;
+  impl_->height_ = img_height;
+  return impl_->model_.Init(model_path, gpu_fraction);
+}
+
+ErrorCode Process(
+    const std::vector<classify::Classification>& classifications,
+    const std::vector<detect::Detection>& detections,
+    std::vector<int>* keyframes) {
+  return kSuccess;
+}
+
+} // namespace count
+} // namespace openem
+

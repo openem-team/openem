503b7253bd296f088179e66ba073b8efb6cecca7
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2018-11-29 11:55:56 -0500

    Store species and aspect ratios in config file

--- train/openem_train/ssd/dataset.py
+++ train/openem_train/ssd/dataset.py
@@ -4,29 +4,7 @@ from typing import List
 import pickle
 import os
 
-VIDEOS_DIR = '../input/train_videos'
-MASKS_DIR = '../output/ruler_masks'
-AVG_MASKS_DIR = '../output/ruler_masks_avg'
-AVG_MASKS_DIR_TEST = '../output/ruler_masks_avg_test'
-RULER_CROPS_DIR = '../output/ruler_crops'
-RULER_CROPS_DIR_TEST = '../output/ruler_crops_test'
-
-MASKS_DIR_TEST = '../output/ruler_masks_test'
-
-SPECIES = ['fourspot', 'grey sole', 'other', 'plaice', 'summer', 'windowpane', 'winter']
-CLASSES = ['_'] + SPECIES
 NB_FOLDS = 4
-
-ASPECT_RATIO_TABLE = {
-    # '_': 0.1,
-    'fourspot': 0.55,
-    'grey sole': 0.55,
-    'other': 0.4,
-    'plaice': 0.55,
-    'summer': 0.5,
-    'windowpane': 0.55,
-    'winter': 0.5
-}
 
 def video_clips(config, is_test=False) -> {str}:
     if is_test:

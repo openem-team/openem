2e48ab70fa736b405ae1eb216cf59124be4498b5
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2018-11-30 13:33:58 -0500

    Comment/cleanup for pylint

--- train/openem_train/util/img_augmentation.py
+++ train/openem_train/util/img_augmentation.py
@@ -1,57 +1,85 @@
+"""Utilities for image augmentation."""
+
 import numpy as np
-import matplotlib.pyplot as plt
-from skimage import color
-from scipy.misc import face
-# from keras.preprocessing import image
-# import PIL.Image
 from scipy.misc import imresize
 
 def grayscale(rgb):
+    """Converts image to grayscale.
+    """
     return rgb.dot([0.299, 0.587, 0.114])
 
 
-def saturation(rgb, variance=0.5, r=None):
-    if r is None:
-        r = np.random.random()
-    gs = grayscale(rgb)
-    alpha = 2 * r * variance
+def saturation(rgb, variance=0.5, mean=None):
+    """Modifies saturation by a random amount.
+    """
+    if mean is None:
+        mean = np.random.random()
+    gray = grayscale(rgb)
+    alpha = 2 * mean * variance
     alpha += 1 - variance
-    rgb = rgb * alpha + (1 - alpha) * gs[:, :, None]
+    rgb = rgb * alpha + (1 - alpha) * gray[:, :, None]
     return np.clip(rgb, 0, 1.0)
 
 
-def brightness(rgb, variance=0.5, r=None):
-    if r is None:
-        r = np.random.random()
+def brightness(rgb, variance=0.5, mean=None):
+    """Modifies brightness by a random amount.
 
-    alpha = 2 * r * variance
+    # Arguments
+        img: Image to modify.
+        variance: Variance in brightness.
+        mean: Mean in brightness, randomly generated if None.
+
+    # Returns
+        Modified image.
+    """
+    if mean is None:
+        mean = np.random.random()
+
+    alpha = 2 * mean * variance
     alpha += 1 - variance
-    # lab = color.rgb2lab(rgb)
-    # lab[:, :, 0] = np.clip(0.0, lab[:, :, 0]*alpha, 1.0)
-    # rgb = color.lab2rgb(lab)
     rgb = rgb * alpha
     return np.clip(rgb, 0, 1.0)
 
 
-def contrast(rgb, variance=0.5, r=None):
-    if r is None:
-        r = np.random.random()
+def contrast(rgb, variance=0.5, mean=None):
+    """Modifies contrast by a random amount.
 
-    gs = grayscale(rgb).mean() * np.ones_like(rgb)
-    alpha = 2 * r * variance
+    # Arguments
+        img: Image to modify.
+        variance: Variance in contrast.
+        mean: Mean in contrast, randomly generated if None.
+
+    # Returns
+        Modified image.
+    """
+    if mean is None:
+        mean = np.random.random()
+
+    gray = grayscale(rgb).mean() * np.ones_like(rgb)
+    alpha = 2 * mean * variance
     alpha += 1 - variance
-    rgb = rgb * alpha + (1 - alpha) * gs
+    rgb = rgb * alpha + (1 - alpha) * gray
     return np.clip(rgb, 0, 1.0)
 
 
-def lighting(img, variance=0.5, r=None):
-    if r is None:
-        r = np.random.randn(3)
+def lighting(img, variance=0.5, mean=None):
+    """Modifies lighting by a random amount.
+
+    # Arguments
+        img: Image to modify.
+        variance: Variance in lighting.
+        mean: Mean in lighting, randomly generated if None.
+
+    # Returns
+        Modified image.
+    """
+    if mean is None:
+        mean = np.random.randn(3)
 
     orig = img.copy
     cov = np.cov(img.reshape(-1, 3) / 1.0, rowvar=False)
     eigval, eigvec = np.linalg.eigh(cov)
-    noise = r * variance
+    noise = mean * variance
     noise = eigvec.dot(eigval * noise) * 1.0
     img += noise
     try:
@@ -62,62 +90,34 @@ def lighting(img, variance=0.5, r=None):
 
 
 def horizontal_flip(img):
+    """Flips an image horizontally.
+    """
     return img[:, ::-1]
 
 
 def vertical_flip(img):
+    """Flips an image vertically.
+    """
     return img[::-1]
 
 
 def blurred_by_downscaling(img, ratio):
-    # ratio = np.random.choice([1, 1, 1, 2, 2.5, 3])
+    """Applies blur to an image by downscaling it.
+
+    # Arguments
+        img: Image to modify.
+        ratio: Ratio to downscale by.
+
+    # Returns
+        Blurred image.
+    """
     resampling = np.random.choice(['nearest', 'lanczos', 'bilinear', 'bicubic'])
 
     if ratio == 1:
         return img
 
-    w = img.shape[1]
-    h = img.shape[0]
+    width = img.shape[1]
+    height = img.shape[0]
     small = imresize(img, ratio, interp=resampling)
-    large = imresize(small, size=(h, w), interp='bilinear').astype(np.float32)/255.0
+    large = imresize(small, size=(height, width), interp='bilinear').astype(np.float32)/255.0
     return large
-
-if __name__ == '__main__':
-    np.random.seed(1337)
-
-    img = face() / 256.0
-
-    import utils
-    utils.print_stats('img', img)
-    utils.print_stats('img_aug', blurred_by_downscaling(img, 0.3))
-    utils.print_stats('img_aug', blurred_by_downscaling(img, 0.3))
-    utils.print_stats('img_aug', blurred_by_downscaling(img, 0.3))
-    utils.print_stats('img_aug', blurred_by_downscaling(img, 0.3))
-
-    plt.imshow(img)
-    plt.figure()
-    # plt.imshow(grayscale(img), cmap='gray')
-    plt.imshow(blurred_by_downscaling(img, 0.3))
-    plt.figure()
-    plt.imshow(blurred_by_downscaling(img, 0.3))
-    plt.figure()
-    plt.imshow(blurred_by_downscaling(img, 0.3))
-    plt.figure()
-    plt.imshow(blurred_by_downscaling(img, 0.1))
-    plt.figure()
-    plt.imshow(blurred_by_downscaling(img, 0.1))
-    plt.show()
-
-    fig, axarr = plt.subplots(4, 3)
-    for i, f in enumerate([saturation, brightness, contrast, lighting]):
-        axarr[i, 0].imshow(f(img, variance=0.8, r=0.0))
-        axarr[i, 1].imshow(f(img, variance=0.8, r=0.5))
-        axarr[i, 2].imshow(f(img, variance=0.8, r=1.0))
-    plt.show()
-
-    fig, axarr = plt.subplots(4, 3)
-    for i, f in enumerate([saturation, brightness, contrast, lighting]):
-        axarr[i, 0].imshow(f(img, variance=0.5))
-        axarr[i, 1].imshow(f(img, variance=0.5))
-        axarr[i, 2].imshow(f(img, variance=0.5))
-    plt.show()

afd2b6d345613b87ddc769ddca936cdc54fc0141
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2019-01-21 19:09:16 -0500

    Get docker image working

--- config/tensorflow-config.cmake
+++ config/tensorflow-config.cmake
@@ -1,7 +1,10 @@
+find_package(Threads REQUIRED)
 add_library(tensorflow SHARED IMPORTED)
 set_target_properties(tensorflow PROPERTIES
   IMPORTED_LOCATION
-  /tensorflow/tensorflow/bazel-bin/tensorflow/libtensorflow_cc.so
+  /tensorflow/bazel-bin/tensorflow/libtensorflow_cc.so
   INTERFACE_INCLUDE_DIRECTORIES
   "/tensorflow/bazel-genfiles;/tensorflow"
+  INTERFACE_LINK_LIBRARIES
+  "/tensorflow/bazel-bin/tensorflow/libtensorflow_framework.so;${CMAKE_THREAD_LIBS_INIT}"
 )

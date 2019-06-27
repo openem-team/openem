fcfa4aa3f6a1564e9eb0a4e2d24d4ee4a79477cf
Brian Tate <brian.tate@cvisionai.com>
2019-06-27 15:18:38 -0400

    Update printouts from stats script

--- scripts/stats.py
+++ scripts/stats.py
@@ -16,15 +16,20 @@ if __name__=="__main__":
 
     train=pd.read_csv(args.train)
     test=pd.read_csv(args.test)
-
+    lenTrain=len(train)
+    lenTest=len(test)
+    percTrain=(float(lenTrain)/(lenTrain+lenTest))*100
+    percTest=(float(lenTest)/(lenTrain+lenTest))*100
+    percTrain=round(percTrain,1)
+    percTest=round(percTest,1)
     fig, (ax1,ax2) = plt.subplots(1,2)
-    fig.suptitle("Population Distribution")
+    fig.suptitle("Population Distribution (by localization)")
     train.hist(column="species_id", ax=ax1)
     test.hist(column="species_id", ax=ax2)
-    ax1.set_title("Training Data")
+    ax1.set_title("Training Data ({} %)".format(percTrain))
     ax1.set_xlabel("Species ID")
     ax1.set_ylabel("Count")
-    ax2.set_title("Test Data")
+    ax2.set_title("Test Data ({} %)".format(percTest))
     ax2.set_xlabel("Species ID")
     ax2.set_ylabel("Count")
     plt.show()

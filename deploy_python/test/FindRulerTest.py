173d4576b5cdaf55f75fcb59ae4089f33a8b6e39
Brian Tate <brian.tate@cvisionai.com>
2019-11-06 13:20:02 -0500

    Switch to tensorflow tests; add logic to verify batch mode

--- deploy_python/test/FindRulerTest.py
+++ deploy_python/test/FindRulerTest.py
@@ -3,34 +3,59 @@ import os
 from openem.FindRuler import RulerMaskFinder
 import cv2
 import numpy as np
+import tensorflow as tf
 
-class FindRulerTest(unittest.TestCase):
+class FindRulerTest(tf.test.TestCase):
     def setUp(self):
         self.deploy_dir = os.getenv('deploy_dir')
         if self.deploy_dir == None:
             raise 'Must set ENV:deploy_dir'
 
         self.ruler_dir = os.path.join(self.deploy_dir, "find_ruler")
-        
-    def test_topLevel(self):
-        pb_file = os.path.join(self.ruler_dir,"find_ruler.pb")
-        finder=RulerMaskFinder(pb_file)
+        self.pb_file = os.path.join(self.ruler_dir,"find_ruler.pb")
 
-        images=['test_image_000.jpg',
-                'test_image_001.jpg',
-                'test_image_002.jpg']
+        # Test image definitions and expected values
+        self.images=['test_image_000.jpg',
+                     'test_image_001.jpg',
+                     'test_image_002.jpg']
+        self.expected_hits = [5663, 2243, 5137]
 
-        
+    def test_stability(self):
+        finder=RulerMaskFinder(self.pb_file)
+
         # Test 1 at a time mode
-        for idx,image in enumerate(images):
+        for idx,image in enumerate(self.images):
             image_data=cv2.imread(os.path.join(self.ruler_dir,
                                            image))
             finder.addImage(image_data)
             image_result = finder.process()
             self.assertIsNotNone(image_result)
-            print(f"Shape = {image_result[0].shape}")
 
-            # Make debug image real quick
-            cv2.imwrite(f"test_{idx}.jpg", image_result[0])
-            
-                                  
+            unique,counts = np.unique(image_result[0], return_counts=True)
+            histogram = dict(zip(unique,counts))
+            hits = histogram[255]
+            # Expect answer within 5%
+            self.assertAlmostEqual(hits, self.expected_hits[idx],
+                                   msg=f"Failed image {idx}: {histogram}",
+                                   delta=hits*.01)
+            # Code here is how to write mask image
+            # cv2.imwrite(f'test_{idx}.jpg', image_result[0])
+
+    def test_batch(self):
+        finder=RulerMaskFinder(self.pb_file)
+        for idx,image in enumerate(self.images):
+            image_data=cv2.imread(os.path.join(self.ruler_dir,
+                                  image))
+            finder.addImage(image_data)
+
+        # Verify the same thing but in batch mode
+        batch_result = finder.process()
+        self.assertIsNotNone(batch_result)
+        self.assertEqual(batch_result.shape[0],len(self.images))
+        for idx in range(len(self.images)):
+            unique,counts = np.unique(batch_result[idx], return_counts=True)
+            histogram = dict(zip(unique,counts))
+            hits = histogram[255]
+            self.assertAlmostEqual(hits, self.expected_hits[idx],
+                                   msg=f"Failed image {idx}: {histogram}",
+                                   delta=hits*.01)
--- deploy_python/test/__main__.py
+++ deploy_python/test/__main__.py
@@ -1,6 +1,8 @@
-import unittest
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
 
 from test.FindRulerTest import FindRulerTest
 
 if __name__=="__main__":
-    unittest.main()
+    tf.test.main()

206d2d4d86b176efcb99f25839dbec3cf41f588b
Brian Tate <brian.tate@cvisionai.com>
2019-11-08 09:52:25 -0500

    Add NMS implementation in Detect.py

--- deploy_python/openem/Detect.py
+++ deploy_python/openem/Detect.py
@@ -21,11 +21,51 @@ class SSDDetector(ImageModel):
     def process(self):
         """ Runs network to find fish in batched images by performing object
             detection with a Single Shot Detector (SSD).
-        
+
         Returns a list of detections (or None if batch is empty)
         """
         result = super(SSDDetector, self).process()
         if result is None:
             return result
 
-        
+
+def nms_boxes(bboxes, scores, score_threshold, nms_threshold, top_k = 0):
+    """ Performs non-maximum supression on a series of overlapping
+        bounding boxes
+
+        bboxes: list of bounding boxes
+        score_threshold a threshold used to filter boxes by score.
+        nms_threshold a threshold used in non maximum suppression.
+        indices the kept indices of bboxes after NMS.
+        top_k if `>0`, keep at most @p top_k picked indices.
+    """
+
+    # Sort the scores in descending order, create a new list keeping
+    # indices and value in a tuple value
+    score_sort_list=[]
+    for idx,score in enumerate(scores):
+        score_sort_list.append((idx,score))
+
+    def second_element(val):
+        return val[1]
+
+    # Sort the new list by the 2nd element (score) in descending order
+    score_sort_list.sort(key = second_element, reverse = True)
+
+    # If only keeping top_k, truncate the list
+    if top_k > 0:
+        score_sort_list[:top_k]
+
+    # Iterate over the scores/boxes and pick winners
+    winning_indices=[]
+    for idx_score in score_sort_list:
+        keep = True
+        for winner in winning_indices:
+            if not keep:
+                break
+            overlap = 1.0 - cv2.jaccardDistance(idx_score[0], winner)
+            keep = overlap <= nms_threshold
+        if keep:
+            winning_indices.append(idx_score[0])
+
+    return winning_indices

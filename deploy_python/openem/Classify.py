f0f64fe81cdb573b410c137f646232ca5d1ffd70
Brian Tate <brian.tate@cvisionai.com>
2019-11-13 15:19:05 -0500

    Add implementation of classify network

---
+++ deploy_python/openem/Classify.py
@@ -0,0 +1,60 @@
+""" Module for performing classification of a detection """
+import numpy as np
+import cv2
+
+from openem.models import ImageModel
+from openem.models import Preprocessor
+from openem.image import crop
+
+from collections import namedtuple
+Classification=namedtuple('Classification', ['species', 'cover'])
+
+class Classifier(ImageModel):
+    preprocessor=Preprocessor(1.0/127.5,
+                              np.array([-1,-1,-1]),
+                              True)
+
+    def __init__(self, model_path, gpu_fraction=1.0):
+        """ Initialize an image model object
+        model_path : str or path-like object
+                     Path to the frozen protobuf of the tensorflow graph
+        gpu_fraction : float
+                       Fraction of GPU allowed to be used by this object.
+        input_name : str
+                     Name of the tensor that serves as the image input
+        output_name : str
+                      Name of the the tensor that serves as the output of
+                      the network
+        """
+        super(Classifier, self).__init__(model_path, gpu_fraction,
+                                       'data:0',
+                                       ['cat_species_1:0',
+                                        'cat_cover_1:0'])
+    def addImage(self, image):
+        """ Add an image to process in the underlying ImageModel after
+            running preprocessing on it specific to this model.
+
+        image: np.ndarray the underlying image (not pre-processed) to add
+               to the model's current batch
+        """
+        return self._addImage(image, self.preprocessor)
+
+    def process(self):
+        tensors = super(Classifier, self).process()
+        if tensors is None:
+            return tensors
+
+        # These tensors are potentially batched by image
+        species = tensors[0]
+        cover = tensors[1]
+
+        results_by_image=[]
+        for image_idx, image_species in enumerate(species):
+            image_cover = cover[image_idx]
+            classification = Classification(species=image_species,
+                                            cover=image_cover)
+            results_by_image.append(classification)
+        return results_by_image
+        
+
+    

5f192d9a6830a8f917ef7e965b2ce968a5ecd259
Brian Tate <brian.tate@cvisionai.com>
2019-11-14 15:29:09 -0500

    Expand skeleton implementation of keyframe finder and test

--- deploy_python/openem/Count.py
+++ deploy_python/openem/Count.py
@@ -7,9 +7,17 @@ from openem.models import ImageModel
 from openem.models import Preprocessor
 from openem.image import crop
 
+KEYFRAME_OFFSET = 32
+MIN_SPACING = 1
+PEAK_THRESHOLD = 0.03
+AREA_THRESHOLD = 0.10
+
 class KeyframeFinder:
     def __init__(self, model_path, img_width, img_height, gpu_fraction=1.0):
-        """ Initialize an image model object
+        """ Initialize a keyframe finder model. Tracks an object detection
+            across frames. In other words associates object detections of the
+            same object across frames of a video.
+
         model_path : str or path-like object
                      Path to the frozen protobuf of the tensorflow graph
         img_width: Width of the image input to detector (pixels)
@@ -31,5 +39,84 @@ class KeyframeFinder:
                     graph_def,
                     return_elements=['input_1:0', 'cumsum_values_1:0'])
 
-    def process(self):
-        pass
+        self.img_width = img_width
+        self.img_height = img_height
+    def normalizeDetection(self, detection):
+        print(detection)
+        """ Normalize a detection coordinates to be relative coordinates """
+        return np.array([detection[0] / self.img_width,
+                         detection[1] / self.img_height,
+                         detection[2] / self.img_width,
+                         detection[3] / self.img_height])
+
+    def process(self, classifications, detections):
+        """ Process the list of classifications and detections, which
+            must be the same length.
+
+            The outer dimension in each parameter is a video; and the inner 
+            each detection in a given video
+
+            classifications: list of list of openem.Classify.Classfication
+            detections: list of list of openem.Detect.Detection
+        """
+        det_len = len(detections)
+        if len(classifications) != len(detections):
+            raise Exception("Classifications / Detections difer in length!")
+
+        # Convert classifications and detections to input required for network
+        seq_len = int(self.input_tensor.shape[1])
+        fea_len = int(self.input_tensor.shape[2])
+        print(f"shape = {self.input_tensor.shape}")
+        input_data = np.zeros((seq_len,fea_len))
+
+        # Add padding before and after sequence based on KEYFRAME_OFFSET
+        input_data[:KEYFRAME_OFFSET,0] = np.ones(KEYFRAME_OFFSET)
+        input_data[det_len:det_len+KEYFRAME_OFFSET,0] = np.ones(KEYFRAME_OFFSET)
+        # Iterate through each frame of the data
+        for idx, detection in enumerate(detections):
+            classification = classifications[idx]
+
+            # We have already padded before and after
+            seq_idx = idx + KEYFRAME_OFFSET
+            # Skip through frames with no detections
+            if len(detection) == 0:
+                input_data[seq_idx][0] = 1.0
+                continue
+
+            # Do a size check on input
+            # We expect either 1 or 2 models per sequence
+            num_species = len(classification.species)
+            num_cover = len(classification.cover)
+            num_loc = len(detection.location)
+            num_fea = num_species + num_cover + num_loc + 2
+            num_of_models = int(fea_len / num_fea)
+
+            if num_of_models != 2 and num_of_models != 1:
+                raise Exception('Bad Feature Length')
+
+            # Layout of the feature is:
+            # Species, Cover, Normalized Location, Confidence, SSD Species
+            # Optional duplicate
+            
+            for model_idx in range(num_of_models):
+                # Calculate indices of vector based on model_idx
+                fea_idx = model_idx * num_fea
+                species_stop = fea_idx + num_species
+                cover_stop = species_stop + num_cover
+                loc_stop = cover_stop + num_loc
+                ssd_conf = loc_stop
+                ssd_species = ssd_conf + 1
+                
+                input_data[seq_idx,fea_idx:species_stop] = \
+                    classification.species
+                input_data[seq_idx,species_stop:cover_stop] = \
+                    classification.cover
+                input_data[seq_idx,cover_stop:loc_stop] = \
+                    self.normalizeDetection(detection.location)
+                input_data[seq_idx, ssd_conf] = detection.confidence
+                input_data[seq_idx, ssd_species] = detection.species
+                
+        result = self.tf_session.run(self.output_tensor,
+                                     feed_dict={self.input_tensor:
+                                                np.array([input_data])})
+        
--- deploy_python/test/CountTest.py
+++ deploy_python/test/CountTest.py
@@ -1,6 +1,10 @@
 import unittest
 import os
 from openem.Count import KeyframeFinder
+
+import openem.Detect
+import openem.Classify
+
 import cv2
 import numpy as np
 import tensorflow as tf
@@ -11,10 +15,28 @@ class ClassifyTest(tf.test.TestCase):
         if self.deploy_dir == None:
             raise 'Must set ENV:deploy_dir'
 
-        self.ruler_dir = os.path.join(self.deploy_dir, "count")
-        self.pb_file = os.path.join(self.ruler_dir,"count.pb")
+        self.classify_dir = os.path.join(self.deploy_dir, "count")
+        self.pb_file = os.path.join(self.classify_dir,"count.pb")
+        self.detections_csv = os.path.join(self.classify_dir,
+                                           "test_detect_sequence.csv")
+        self.classification_csv = os.path.join(self.classify_dir,
+                                           "test_classify_sequence.csv")
 
     def test_correctness(self):
         # Test files have dims of 720,360
         finder=KeyframeFinder(self.pb_file, 720, 360)
         self.assertIsNotNone(finder)
+        detections = openem.Detect.IO.from_csv(self.detections_csv)
+        classifications = openem.Classify.IO.from_csv(self.classification_csv)
+        self.assertEqual(len(detections), 104)
+        self.assertEqual(len(detections), len(classifications))
+        keyframes = finder.process(classifications,detections)
+
+    def test_errorChecks(self):
+        finder=KeyframeFinder(self.pb_file, 720, 360)
+        raised=False
+        try:
+            finder.process([1,2,3],[1,2])
+        except Exception as e:
+            raised=True
+        self.assertTrue(raised)

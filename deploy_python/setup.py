1ee5028715e9c1d6e78c8f901d19b1500c4b3a89
Brian Tate <brian.tate@cvisionai.com>
2019-11-17 18:40:13 -0500

    Add setup.py for openem

---
+++ deploy_python/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='openem',
+    version='0.2.0',
+    url='https://github.com/openem-team/openem',
+    author='CVision AI',
+    author_email='info@cvisionai.com',
+    maintainer='CVision AI',
+    maintainer_email='info@cvisionai.com',
+    packages=['openem'],
+    # Don't require keras_retina net as that is only for
+    # optional detector model, and not on pypi
+    install_requires=['tensorflow-gpu>=1.14.0,<2.0.0']
+)

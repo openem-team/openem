2651b3681136a58c652686116cef0c729827a4e3
Brian Tate <brian.tate@cvisionai.com>
2019-11-28 09:20:24 -0500

    Add progressbar dependency to deploy library

--- deploy_python/setup.py
+++ deploy_python/setup.py
@@ -11,5 +11,5 @@ setuptools.setup(
     packages=['openem', 'openem.Detect'],
     # Don't require keras_retina net as that is only for
     # optional detector model, and not on pypi
-    install_requires=['tensorflow-gpu>=1.14.0,<2.0.0']
+    install_requires=['tensorflow-gpu>=1.14.0,<2.0.0', 'progressbar2>=3.43.1']
 )

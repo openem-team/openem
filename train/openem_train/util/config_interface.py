31a93aafeb3ab7af4361b6026b6feee5da2324bc
Brian Tate <brian.tate@cvisionai.com>
2019-12-13 16:36:57 -0500

    Handle pngs correctly

--- train/openem_train/util/config_interface.py
+++ train/openem_train/util/config_interface.py
@@ -429,7 +429,8 @@ class ConfigInterface:
         """Returns list of all training roi images.
         """
         patt = os.path.join(self.train_rois_dir(), '**', '*.jpg')
-        return glob.glob(patt, recursive=True)
+        patt2 = os.path.join(self.train_rois_dir(), '**', '*.png')
+        return glob.glob(patt, recursive=True).extend(glob.glob(patt2, recursive=True)
 
     def train_dets(self):
         """Returns list of all training detection images.

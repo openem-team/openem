1da959261fcc11c0495498adf8bd16c6e6fb272b
Brian Tate <brian.tate@cvisionai.com>
2019-12-13 14:02:54 -0500

    Clean up preprocessing steps for retinanet network.

--- deploy_python/openem/Detect/RetinaNet.py
+++ deploy_python/openem/Detect/RetinaNet.py
@@ -6,46 +6,57 @@ from openem.models import ImageModel
 import cv2
 
 from openem.Detect import Detection
+from openem.image import force_aspect
 
-class SubtractMeanImage:
-    """ Subtract a mean image from the input
+class RetinaNetPreprocessor:
+    """ Perform preprocessinig for RetinaNet inputs
         Meets the callable interface of openem.Detect.Preprocessor
     """
-    def __init__(self,meanImage):
+    def __init__(self,meanImage=None):
         self.mean_image = meanImage
 
     def __call__(self, image, requiredWidth, requiredHeight):
-        #Reverse color channels
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        #TODO: (Provide way to optionally convert channel ordering?)
+        #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = image.astype(np.float32)
+
+        required_aspect = requiredWidth / requiredHeight
+        image = force_aspect(image, required_aspect)
         resized_image = cv2.resize(image, (requiredWidth, requiredHeight))
         if self.mean_image:
             for dim in [0,1,2]:
                 resized_image[:,:,dim] -= self.mean_image[:,:,dim]
+        else:
+            # Use the ImageNet mean image by default; which in BGR is:
+            imagenet_mean = np.array([103.939, 116.779, 123.68 ])
+            resized_image -= imagenet_mean
         return resized_image
 
-NETWORK_IMAGE_SHAPE=(720,1280)
 class RetinaNetDetector(ImageModel):
-    def __init__(self, modelPath, meanImage=None, gpuFraction=1.0):
+    def __init__(self, modelPath, meanImage=None, gpuFraction=1.0, imageShape=(360,720)):
         """ Initialize the RetinaNet Detector model
         modelPath: str
                    path-like object to frozen pb graph
         meanImage: np.array
                    Mean image subtracted from image prior to network
                    insertion. Can be None.
+        image_shape: tuple
+                   (height, width) of the image to feed into the detector network.
         """
         super(RetinaNetDetector,self).__init__(modelPath,
                                                gpuFraction,
                                                'input_1:0',
                                                'nms/map/TensorArrayStack/TensorArrayGatherV3:0')
-        self.input_shape[1:3] = NETWORK_IMAGE_SHAPE
+        self.input_shape[1:3] = imageShape
+
+        self.image_shape = imageShape
 
         if meanImage:
-            resized_mean = cv2.resize(meanImage,(NETWORK_IMAGE_SHAPE[1],
-                                                 NETWORK_IMAGE_SHAPE[0]))
-            self.preprocessor=SubtractMeanImage(resized_mean)
+            resized_mean = cv2.resize(meanImage,(imageShape[1],
+                                                 imageShape[0]))
+            self.preprocessor=RetinaNetPreprocesser(meanImage=resized_mean)
         else:
-            self.preprocessor=SubtractMeanImage(None)
+            self.preprocessor=RetinaNetPreprocessor(meanImage=None)
 
         self._imageSizes = None
 
@@ -62,14 +73,14 @@ class RetinaNetDetector(ImageModel):
         # clip to image shape
         detections[:, :, 0] = np.maximum(0, detections[:, :, 0])
         detections[:, :, 1] = np.maximum(0, detections[:, :, 1])
-        detections[:, :, 2] = np.minimum(NETWORK_IMAGE_SHAPE[1], detections[:, :, 2])
-        detections[:, :, 3] = np.minimum(NETWORK_IMAGE_SHAPE[0], detections[:, :, 3])
+        detections[:, :, 2] = np.minimum(self.image_shape[1], detections[:, :, 2])
+        detections[:, :, 3] = np.minimum(self.image_shape[0], detections[:, :, 3])
 
         num_images = detections.shape[0]
         for idx in range(num_images):
             # correct boxes for image scale
-            h_scale = NETWORK_IMAGE_SHAPE[0] / self._imageSizes[idx][0]
-            w_scale = NETWORK_IMAGE_SHAPE[1] / self._imageSizes[idx][1]
+            h_scale = self.image_shape[0] / self._imageSizes[idx][0]
+            w_scale = self.image_shape[1] / self._imageSizes[idx][1]
 
             detections[idx, :, 0] /= w_scale
             detections[idx, :, 1] /= h_scale
@@ -87,7 +98,7 @@ class RetinaNetDetector(ImageModel):
         # compute predicted labels and scores
         num_imgs = detections.shape[0]
         for img_idx in range(num_imgs):
-            if frame:
+            if not frame is None:
                 this_frame = frame + img_idx
             else:
                 this_frame = None
--- deploy_python/openem/image.py
+++ deploy_python/openem/image.py
@@ -1,6 +1,7 @@
 """ Utility module for dealing with image operations """
 
 import numpy as np
+import math
 
 def crop(image, roi):
     """ Returns a *copy* of the region of interest from the image
@@ -15,3 +16,67 @@ def crop(image, roi):
     y1=int(roi[1]+roi[3])
     cropped=np.copy(image[y0:y1,x0:x1])
     return cropped
+
+def resize_and_fill(image, desired_shape):
+    """
+    Resize an image to a desired shape (height,width) and maintaining
+    the aspect ratio. Fill any extra areas with black.
+
+    :param image: ndarray of the image
+    :param desired_shape: tuple describing the output shape
+
+    :returns image_resized,scaleFactor:
+
+    image_resized is ndarray represented the scaled + padded image
+
+    scaleFactor Given a pixel coordinate in the original this factor
+                should be applied to land on the new image.
+
+    """
+    image_height=image.shape[0]
+    image_width=image.shape[1]
+    image_channels=image.shape[2]
+
+    desired_width=desired_shape[1]
+    desired_height=desired_shape[0]
+    growth_factor=min(desired_height/image_height,
+                           desired_width/image_width)
+    new_height=int(image_height*growth_factor)
+    new_width=int(image_width*growth_factor)
+    image_resized,_=resize_image(image,new_height,new_width)
+    added_rows=desired_height-new_height
+    added_cols=desired_width-new_width
+
+    if added_rows:
+        black_bar=np.zeros((added_rows, new_width, image_channels))
+        image_resized = np.append(image_resized,black_bar, axis=0)
+
+    if added_cols:
+        black_bar=np.zeros((new_height, added_cols, image_channels))
+        image_resized = np.append(image_resized,black_bar, axis=1)
+
+    scaleFactor=(float(new_height)/image_height,float(new_width)/image_width)
+    return image_resized, scaleFactor
+
+def force_aspect(image, required_aspect_ratio):
+    """ Given an image; force it to be given aspect ratio prior to resizing """
+    img_height = image.shape[0]
+    img_width = image.shape[1]
+    img_aspect = img_width / img_height
+    if math.isclose(required_aspect_ratio, img_aspect):
+        return image
+
+    if img_aspect < required_aspect_ratio:
+        # This is when the image is boxier than the aspect ratio
+        # so we add a black bar at the right side to compensate
+        # this added bar does not effect annotation coordinates
+        new_img_width = round(img_height * required_aspect_ratio)
+        image,sf = resize_and_fill(image, (img_height, new_img_width))
+    else:
+        # This is when the image is narrower than the aspect ratio
+        # so we add a black bar at the bottom to compensate
+        # this added bar does not effect annotation coordinates
+        new_img_height = round(img_width / required_aspect_ratio)
+        image,sf = resize_and_fill(image, (new_img_height, img_width))
+    assert math.isclose(sf[0],1.0) and math.isclose(sf[1],1.0)
+    return image

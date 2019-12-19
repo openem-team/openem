6a8997a4fcd7a137d928d5432908274bc4ca978c
Brian Tate <brian.tate@cvisionai.com>
2019-12-18 19:52:03 -0500

    Make graph 2 plots rather than one fix axes.

--- scripts/make_pr_graph.py
+++ scripts/make_pr_graph.py
@@ -19,17 +19,24 @@ if __name__=="__main__":
     recall = matrix[:,2]
     doubles = matrix[:,3]
 
-    fig,ax1 = plt.subplots()
+    fig,axes = plt.subplots(2)
+    ax1 = axes[0]
     ax1.plot(recall, precision, color='blue', label='Precision/Recall')
     ax1.set_xlabel('Recall')
-    ax1.plot(recall, keep_t, color='green', label='Keep Threshold')
-
-    
+    ax1.set_ylabel('Precision')
+    #ax1.plot(recall, keep_t, color='green', label='Keep Threshold')
+    ax1.set_xlim(0,1)
+    ax1.set_ylim(0,1)
+    ax2 = axes[1]
+    ax2.plot(recall, keep_t,color='green', label='Keep Threshold')
+    ax2.set_ylabel('Keep Threshold')
+    ax2.set_xlabel('Recall')
+    ax2.set_xlim(0,1)
+    ax2.set_ylim(0,1)
     if not args.doubles:
         ax2 = ax1.twinx()
         ax2.plot(recall, doubles, color='red', label='doubles')
-        ax2.set_ylabel('doubles', color='red')
-        ax2.set_yscale('log')
+        ax2.set_ylabel('Doubles / Total Truth', color='red')
         ax2.tick_params(axis='y', labelcolor='red')
 
     fig.legend(loc='lower left')

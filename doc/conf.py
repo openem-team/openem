ccea0a5166c8d2f94d43f1ab2f8245cb0f6b0b57
Brian Tate <brian.tate@cvisionai.com>
2019-12-31 20:15:37 -0500

    update mocks

--- doc/conf.py
+++ doc/conf.py
@@ -182,4 +182,4 @@ html_theme = 'sphinx_rtd_theme'
 github_url='https://github.com/openem-team/openem'
 
 autoclass_content = 'both'
-autodoc_mock_imports = ['tensorflow']
+autodoc_mock_imports = ['tensorflow', 'cv2', 'progressbar2']

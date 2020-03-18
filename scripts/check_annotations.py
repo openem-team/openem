b47ca5d50b1145eb4d18db1865e5cf4d4e105d26
Ryan Kopec <ryankopec91@gmail.com>
2020-03-17 21:05:12 -0400

    Update check_annotation to work with more CSV files.

--- scripts/check_annotations.py
+++ scripts/check_annotations.py
@@ -14,22 +14,45 @@ def read_args():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("-i", "--input", help="Input CSV file.")
     parser.add_argument("-o", "--output", help="Output CSV file.")
+    parser.add_argument("--header", help="Set if you know there is a header in the CSV file. Default is False.", action='store_true')
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = read_args()
+    if args.header:
+        annotations = pd.read_csv(args.input)
+        columns = list(annotations.columns)
+        indices = []
 
-    annotations = pd.read_csv(args.input, header=0)
+        if ("width" in columns) and ("height" in columns):
+            for idx, row in annotations.iterrows():
+                if row["width"] == 0:
+                    indices.append(idx)
+                elif row["height"] == 0:
+                    indices.append(idx)
+        if ("x1" in columns) and ("x2" in columns) and ("y1" in columns) and ("y2" in columns):
+            for idx, row in annotations.iterrows():
+                if row["x1"] >= row["x2"]:
+                    indices.append(idx)
+                elif row["y1"] >= row["y2"]:
+                    indices.append(idx)
 
-    indices = []
-    for idx, row in annotations.iterrows():
-        if row[2] >= row[4]:
-            indices.append(idx)
-        elif row[1] >= row[3]:
-            indices.append(idx)
-    print(f"Removing indices: {indices}")
-    
-    updated_annotations = annotations.drop(indices)
+        print(f"Removing indices: {indices}")
+        
+        updated_annotations = annotations.drop(indices)
+
+    else:
+        annotations = pd.read_csv(args.input, header=None)
+
+        indices = []
+        for idx, row in annotations.iterrows():
+            if row[2] >= row[4]:
+                indices.append(idx)
+            elif row[1] >= row[3]:
+                indices.append(idx)
+        print(f"Removing indices: {indices}")
+        
+        updated_annotations = annotations.drop(indices)
 
     if args.output:
         updated_annotations.to_csv(args.output, index=False)

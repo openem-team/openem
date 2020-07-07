aba587dd50d4a74e9cc02672463d23886f76ba79
Brian Tate <brian.tate@cvisionai.com>
2020-07-07 16:29:48 -0400

    Support track upload with new api

--- scripts/analysis/upload_pairs.py
+++ scripts/analysis/upload_pairs.py
@@ -4,6 +4,7 @@
 
 import argparse
 import logging
+import math
 import sys
 
 import progressbar
@@ -17,37 +18,53 @@ logger = logging.getLogger("upload_pairs.py")
 logging.basicConfig(level=logging.INFO)
 
 count = 0
-def upload_pair(pair, progress_bar):
+def upload_pair(pair, progress_bar, results):
     global count
     count += 1
     progress_bar.update(count)
     obj = {"localization_ids" : [pair['first'], pair['second']],
+           "type": args.state_type_id,
+           "media_ids" : [pair['media']],
            "Species" : pair.species,
            "IoU" : float(pair.iou),
-           "Match": "Unknown",
-           "type": args.state_type_id,
-           "media_ids" : pair['media']}
-    tator.Track.new(obj)
+           "Match": "Unknown"
+    }
+    if args.version_id:
+        obj.update({"version": args.version_id})
+    results.append(obj)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser = pytator.tator.cli_parser(parser)
     parser.add_argument("--state-type-id",
-                        help="ID to use for pair ingestion")
+                        help="ID to use for pair ingestion",
+                        type=int)
+    parser.add_argument("--version-id",
+                        help="ID for the version to upload",
+                        type=int)
     parser.add_argument("pairs_csv",
                         help="output file")
 
     args = parser.parse_args()
 
     pairs_df = pd.read_csv(args.pairs_csv)
-    
+
     tator = pytator.Tator(args.url, args.token, args.project)
-    
+
     pBar = progressbar.ProgressBar(redirect_stdout=True,
                                    redirect_stderr=True,
                                    max_value=len(pairs_df))
     pBar.update(0)
-    pairs_df.apply(upload_pair, axis=1, args=(pBar,))
+    results=list()
+    pairs_df.apply(upload_pair, axis=1, args=(pBar,results))
     pBar.finish()
 
-    
+    batch_size = 50
+    batch_count = math.ceil(len(results) / batch_size)
+    pBar = progressbar.ProgressBar(redirect_stdout=True,
+                                   redirect_stderr=True,
+                                   max_value=batch_count)
+    for batch_idx in pBar(range(batch_count)):
+        start = batch_idx * batch_size
+        end = start + batch_size
+        tator.Track.new(results[start:end])

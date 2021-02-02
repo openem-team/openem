c409cc89665b0105444dfd2d1a21944e8caff3c2
Hugh Enxing <hugh.enxing@cvisionai.com>
2021-02-02 09:15:51 -0500

    Adds feature extractor
    This feature extraction workflow runs on videos and extracts a 1x2048
    vector of features for each frame in the video. It also uploads the
    extracted features to s3 as an hdf file and adds the bucket and object
    key as an attribute of the media object for later retrieval and
    analysis. Currently, the feature extracted is a placeholder for testing
    and will be filled in later with a more meaningful calculation.

---
+++ scripts/tator/feature_extraction_entry.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import json
+import logging
+import os
+import subprocess
+import shutil
+import sys
+
+
+log_filename = "feature_extraction_entry.log"
+
+
+logging.basicConfig(
+    handlers=[logging.FileHandler(log_filename, mode="w"), logging.StreamHandler()],
+    format="%(asctime)s %(levelname)s:%(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+    level=logging.INFO,
+)
+
+
+logger = logging.getLogger(__name__)
+
+
+if __name__ == "__main__":
+    # TODO remove pipeline_args_str if unneeded
+    pipeline_args_str = os.getenv("TATOR_PIPELINE_ARGS")
+    if pipeline_args_str:
+        pipeline_args = json.loads(pipeline_args_str)
+    else:
+        pipeline_args = {}
+
+    args = [
+        "python3",
+        "/scripts/tator/feature_extractor.py",
+        "--host",
+        os.getenv("TATOR_API_SERVICE").replace("/rest", ""),
+        "--token",
+        os.getenv("TATOR_AUTH_TOKEN"),
+        "--access-key",
+        os.getenv("OBJECT_STORAGE_ACCESS_KEY", ""),
+        "--secret-key",
+        os.getenv("OBJECT_STORAGE_SECRET_KEY", ""),
+        "--s3-bucket",
+        os.getenv("S3_BUCKET", ""),
+        "--endpoint-url",
+        os.getenv("ENDPOINT_URL"),
+        "--work-dir",
+        os.getenv("TATOR_WORK_DIR"),
+        "--project-id",
+        os.getenv("TATOR_PROJECT_ID"),
+        "--attribute-name",
+        os.getenv("TATOR_ATTRIBUTE_NAME"),
+        "--media-ids",
+        *os.getenv("TATOR_MEDIA_IDS").split(","),
+    ]
+
+    cmd = " ".join(args)
+    logger.info(f"Feature Extraction Command = '{cmd}'")
+    p = subprocess.Popen(args)
+    p.wait()
+    sys.exit(p.returncode)
